79b4273ea096f278f6c37cd0e985e437b7f409cb
putafroginit <enavarra3973@student.pps.net>
2026-09-10 11:43:49 -0700

    additions to combat test, creation of text file and fixing of set health function

--- simple-complexity-project-2026-27/Scripts/Combat.gd
+++ simple-complexity-project-2026-27/Scripts/Combat.gd
@@ -1,40 +1,51 @@
 extends Control
 
-var Cphase = 0
+@export var Cphase = 0
+@export var Enemy_health = 0
+@export var Enemy_max_health = 0
+@export var Enemy2_health = 0
+@export var Enemy2_max_health = 0
+@export var Enemy3_health = 0
+@export var Enemy3_max_health = 0
+
+
 
 func _ready() -> void:
-	set_player_health()			#Sets the player health at the start of combat to actually equal the players actual health 
+	set_health($ProgressBar , State.Health , State.Max_Health)			#Sets the player health at the start of combat to actually equal the players actual health 
+	Enemy_health = 
+	Enemy_max_health = 
+	set_health($Enemy_Container/ProgressBar , Enemy_health , Enemy_max_health)
 	Cphase = 1
-func set_player_health():
-	$ProgressBar.value = State.Health 
-	$ProgressBar.max_value = State.Max_Health
-	$ProgressBar/Label.text = "HP:%d/%d" % [State.Health, State.Max_Health]
-
-func set_enemy_health():
-	$Enemy_Container/ProgressBar/Label.text = "HP:%d/%d" % [$Enemy_Container/ProgressBar.value, $Enemy_Container/ProgressBar.max_value]
 	
+	
+func set_health(progressbar , health , max_health):
+	progressbar.value = health 
+	
+	progressbar.max_value = max_health
+	
+	progressbar.get_node("Label").text = "HP:%d/%d" % [health, max_health]
+
+
 func _on_action_1_pressed() -> void:
 	if Cphase == 1:
-		$Enemy_Container/ProgressBar.value -= 1 
-		set_enemy_health()
-		Cphase = 2
+		Enemy_health -= 1
+		set_health($Enemy_Container/ProgressBar , Enemy_health , Enemy_max_health)
+		Cphase = 1
 	else:
 		pass
 
 
 func _on_action_2_pressed() -> void:
 	if Cphase == 1:
-		$Enemy_Container/ProgressBar.value -= 2 
-		set_enemy_health()
-		Cphase = 2
+		set_health($Enemy_Container/ProgressBar , Enemy_health - 2 , Enemy_max_health)
+		Cphase = 1
 	else:
 		pass
 
 
 func _on_action_3_pressed() -> void:
 	if Cphase == 1:
-		$Enemy_Container/ProgressBar.value -= 3 
-		set_enemy_health()
+		set_health($Enemy_Container/ProgressBar , Enemy_health - 3 , Enemy_max_health)
 		Cphase = 2
 	else:
 		pass
